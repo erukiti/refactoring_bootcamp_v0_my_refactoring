759229d005e94490373dc27d53ccc6e6a9ea3e76
SASAKI Shunsuke (erukiti) <erukiti@gmail.com>
2012-09-10 17:54:30 +0900

    flow: 不備によりコミットが消えたがソースは残ってたのでコミット

--- flow/battle.rb
+++ flow/battle.rb
@@ -1,8 +1,10 @@
 # coding: utf-8
 
+class DeadException < Exception ; end
+class DarkException < Exception ; end
+
 class Character
-  attr_accessor :life
-  attr_reader :atack
+  attr_reader :life
   attr_reader :defence
   attr_reader :element
 
@@ -13,33 +15,30 @@ class Character
     @element = element
   end
 
-  def atack(defencer)
-    if @element == :dark || defencer.element == :dark
-        return false
-    elsif @atack > defencer.defence
-      damage = @atack - defencer.defence
-      is_elemental_bonus = false
-
-      if @element != defencer.element
-        if (@element == :water || defencer.element == :water) && 
-           (@element == :fire || defencer.element == :fire)
-          is_elemental_bonus = true
-        end
-        if (@element == :wind || defencer.element == :wind) && 
-           (@element == :earth || defencer.element == :earth)
-          is_elemental_bonus = true
-        end
-      end
-      damage *= 2 if is_elemental_bonus
-      if defencer.life < damage
-        defencer.life = 0
-      else
-        defencer.life -= damage
-      end
-    end
-    true
+  def elemental_bonus?(defencer)
+    (@element == :water && defencer.element == :fire) ||
+    (@element == :fire && defencer.element == :water) ||
+    (@element == :wind && defencer.element == :earth) ||
+    (@element == :earth && defencer.element == :wind)
   end
 
+  def get_damage(damage)
+    if @life <= damage
+      @life = 0
+      raise DeadException
+    else
+      @life -= damage
+    end
+  end
+
+  def atack(defencer)
+    raise DarkException if @element == :dark || defencer.element == :dark
+    return if @atack <= defencer.defence
+
+    damage = @atack - defencer.defence
+    damage *= 2 if elemental_bonus?(defencer)
+    defencer.get_damage(damage)
+  end
 end
 
 class Battle
@@ -53,10 +52,22 @@ class Battle
 
   def start
     loop do
-      return nil if !@first.atack(@second)
-      return @first if @second.life <= 0
-      return nil if !@second.atack(@first)
-      return @second if @first.life <= 0
+      begin
+        @first.atack(@second)
+      rescue DeadException
+        return @first
+      rescue DarkException
+        return nil
+      end
+
+      begin
+        @second.atack(@first)
+      rescue DeadException
+        return @second
+      rescue DarkException
+        return nil
+      end
+
       @turn += 1
     end
   end
